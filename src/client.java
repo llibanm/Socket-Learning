c933ec6d75446df4f9ff59a631e38a244fd95ff2
llibanm <libanmohamedali61@gmail.com>
2025-04-30 14:52:51 +0200

    client can now connect to myServerSocket succesfully

--- src/client.java
+++ src/client.java
@@ -6,21 +6,45 @@ import java.net.Socket;
 
 public class client {
 
-    private Socket socket;
+    private Socket socket; // to connect itself to myServerSocket
     private int Port;
+
     private BufferedReader in;
     private PrintWriter out;
+
     public client(int port) {
         this.Port = port;
     }
 
-    public void start() throws IOException {
+    public void start() {
 
-        this.socket = new Socket("localhost", this.Port);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
+        try {
+            System.out.println("[CLIENT] Engaging connection...");
+            this.socket = new Socket("localhost", this.Port);
+            System.out.println("[CLIENT] Connection established on address : " + this.socket.getInetAddress().getHostAddress()+" and port : "+this.socket.getPort());
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.println("[CLIENT] Now quitting connection...");
+            in.close();
+            out.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
+    }
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Usage: client <port>");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        client client = new client(port);
+        client.start();
     }
 
 
