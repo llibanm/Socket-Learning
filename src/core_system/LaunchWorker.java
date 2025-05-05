f3119693573d903eaefe981d517b35abeccb0dd6
llibanm <libanmohamedali61@gmail.com>
2025-05-05 16:09:42 +0200

    finalized worker and master base code

--- src/core_system/LaunchWorker.java
+++ src/core_system/LaunchWorker.java
@@ -1,4 +1,46 @@
 package core_system;
 
+import java.io.IOException;
+
 public class LaunchWorker {
+
+    private Worker[] workersTab;
+    private Thread[] workerThreadsTab;
+
+    public LaunchWorker(int nbrWorkers) {
+        workersTab = new Worker[nbrWorkers];
+        workerThreadsTab = new Thread[nbrWorkers];
+    }
+
+    public void start(){
+        try {
+            for(int i = 0; i < workersTab.length; i++){
+                workersTab[i] = new Worker(i);
+                workerThreadsTab[i] = new Thread(workersTab[i]);
+                workerThreadsTab[i].start();
+            }
+            for(int i = 0; i < workerThreadsTab.length; i++){
+                workerThreadsTab[i].join();
+            }
+        }catch ( InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Worker[] getWorkersTab() {
+        return workersTab;
+    }
+
+    public Thread[] getWorkerThreadsTab() {
+        return workerThreadsTab;
+    }
+
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("Usage: java MasterNIO <Number of worker>");
+            return;
+        }
+        LaunchWorker launchWorker = new LaunchWorker(Integer.parseInt(args[0]));
+        launchWorker.start();
+    }
 }
--- src/core_system/MasterNIO.java
+++ src/core_system/MasterNIO.java
@@ -9,13 +9,9 @@ public class MasterNIO {
     private final int port = 2000;
 
 
-    private Worker[] workersTab;
-    private Thread[] workerThreadsTab;
 
-    public MasterNIO(int nbrWorkers) {
-        this.workersTab = new Worker[nbrWorkers];
-        this.workerThreadsTab = new Thread[nbrWorkers];
-    }
+
+    public MasterNIO() {}
 
     public void start() {
             serverChannel = new MyServerSelector(this.port);
@@ -26,14 +22,6 @@ public class MasterNIO {
             System.out.println("[MasterNIO] Lancement du serveur...");
             serverThread.start();
             serverThread.join();
-            System.out.println("[MasterNIO] Lancement des workers");
-            Thread.sleep(1);
-            for (int i = 0; i < workersTab.length; i++) {
-                workersTab[i] = new Worker(i++);
-                workerThreadsTab[i] = new Thread(workersTab[i]);
-                workerThreadsTab[i].start();
-                workerThreadsTab[i].join();
-            }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -47,22 +35,10 @@ public class MasterNIO {
         return serverChannel;
     }
 
-    public Worker[] getWorkersTab() {
-        return workersTab;
-    }
-
-    public Thread[] getThreadsTab() {
-        return workerThreadsTab;
-    }
 
     public static void main(String[] args) {
 
-        if(args.length != 1){
-            System.out.println("Usage: java <Number of workers to instantiate>");
-            System.exit(1);
-        }
-
-        MasterNIO nio = new MasterNIO(Integer.parseInt(args[0]));
+        MasterNIO nio = new MasterNIO();
         nio.start();
 
     }
