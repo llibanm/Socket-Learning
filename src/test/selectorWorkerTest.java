2f844ff526d5e2ad5ff7f74d1f3e5762372a5249
llibanm <libanmohamedali61@gmail.com>
2025-05-09 15:50:18 +0200

    reworking selection sockets on seocketServerTest and selectorWorkerTest in order to improve my code and understanding

---
+++ src/test/selectorWorkerTest.java
@@ -0,0 +1,100 @@
+package test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Set;
+
+public class selectorWorkerTest {
+
+    public static void main(String[] args) throws IOException {
+
+        Selector selectorWorker = Selector.open();
+        SocketChannel socketChannelWorker = SocketChannel.open();
+        socketChannelWorker.configureBlocking(false);
+
+        System.out.println("[WORKER] : connection to server");
+
+        socketChannelWorker.connect(new InetSocketAddress("localhost",2000)); // connection to localhost server on port 2000
+        socketChannelWorker.register(selectorWorker, SelectionKey.OP_CONNECT); //
+
+
+
+        while (true) { // tant que la connection est etablie
+
+            int ready = selectorWorker.select();
+            if(ready == 0){
+                continue;
+            }
+
+
+
+            Set<SelectionKey> keys = selectorWorker.selectedKeys();
+            Iterator<SelectionKey> iterator = keys.iterator();
+
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+
+                    if (key.isConnectable()) {
+
+                        if(socketChannelWorker.finishConnect()){
+                            System.out.println("[WORKER] : ACCEPTED connection to server: "+socketChannelWorker.getRemoteAddress());
+                        }
+
+                        SocketChannel secondChannelWorker = (SocketChannel) key.channel();
+
+                        secondChannelWorker.register(selectorWorker, SelectionKey.OP_READ);
+
+                        System.out.println("[WORKER] : READ connection from: "+secondChannelWorker.getRemoteAddress());
+                    }
+                    else if (key.isReadable()) {
+                        try {
+                            ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.BYTES);// will allocate the exact number of bytes necessary
+
+                            int readBytes = socketChannelWorker.read(byteBuffer);
+                            if(readBytes == -1){
+                                System.out.println("[WORKER] : Read Failed");
+                                System.out.println("[WORKER] : Connection closed");
+                                key.cancel();
+                                return;
+                            }
+
+                            byteBuffer.flip();
+                            int size = byteBuffer.getInt();
+
+                            byteBuffer =ByteBuffer.allocate(size); // re allocate the exact number of bytes necessary
+
+                            readBytes = socketChannelWorker.read(byteBuffer);
+                            if(readBytes == -1){
+                                System.out.println("[WORKER] : Read Failed");
+                                System.out.println("[WORKER] : Connection closed");
+                                key.cancel();
+                                return;
+                            }
+                            byteBuffer.flip();
+                            byte[] message = new byte[byteBuffer.remaining()];
+                            byteBuffer.get(message);
+                            String messageString = new String(message, StandardCharsets.UTF_8);
+                            System.out.println("[WORKER] : Received: " + messageString);
+                            key.interestOps(SelectionKey.OP_WRITE);// changing read rights to write rights
+                            System.out.println("[WORKER] : WRITE OK");
+                        }catch (IOException e){
+                            e.printStackTrace();}
+                    }
+                    else if (key.isWritable()) {
+
+                    }
+
+            }
+        }
+
+    }
+
+}
--- src/test/socketServerTest.java
+++ src/test/socketServerTest.java
@@ -4,23 +4,89 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class socketServerTest {
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(9999)) {
-            System.out.println("Server is listening on port 99");
-            Socket socket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        try {
 
-            String line = in.readLine();
-            System.out.println("[Server] Recieved : "+line);
-            out.println("Hello from server");
-            socket.close();
+
+
+            Selector selector = Selector.open();
+
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress("localhost", 2000)); // server open
+
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT); // server register incoming key on status OP_ACCEPT
+
+            System.out.println("[SERVER] : SERVER open on port 2000");
+
+            while (true) {
+
+                int readyChannels = selector.select();
+
+                if (readyChannels == 0) { // if no channel available, reran looping
+                    continue;
+                }
+
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    iterator.remove();
+
+                    if (key.isAcceptable()) {
+                        SocketChannel socketChannel = ((ServerSocketChannel)key.channel()).accept(); // handleAccept part
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector, SelectionKey.OP_WRITE); // change OP_ACCEPT into OP_WRITE, server will write to incoming connections
+                        System.out.println("[SERVER] : SERVER accepted connection from: "+socketChannel.getRemoteAddress());
+                        System.out.println("[SERVER] : sending data");
+                    }
+                    else if (key.isWritable()) {
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        String message = "Hello Worker";
+
+                        byte[] messageBytes = message.getBytes();
+                        int messageLength = messageBytes.length;
+
+                        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES+messageLength);// allocate the exact number of byte needed
+                        buffer.putInt(messageLength);
+                        buffer.put(messageBytes);
+                        buffer.flip();
+
+                        try {
+                            socketChannel.write(buffer);
+
+                            if(!buffer.hasRemaining()) {
+                                System.out.println("[SERVER] : send data: "+message+" to "+socketChannel.getRemoteAddress());
+                                key.interestOps(SelectionKey.OP_READ);// change write rights to read rights
+                            }
+
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
