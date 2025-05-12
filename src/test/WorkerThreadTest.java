0a77fe09a6c5b8656448930054c79ffd5c3ab424
llibanm <libanmohamedali61@gmail.com>
2025-05-12 09:35:49 +0200

    now added tesst for multiple worker in threads

---
+++ src/test/WorkerThreadTest.java
@@ -0,0 +1,46 @@
+package test;
+
+public class WorkerThreadTest {
+
+    private selectorWorkerTest[] workers;
+    private Thread[] workerThreads;
+
+    public WorkerThreadTest() {
+        workers = new selectorWorkerTest[3];
+        workerThreads = new Thread[workers.length];
+    }
+
+    public void start() {
+        try {
+            for (int i = 0; i < workers.length; i++) {
+                workers[i] = new selectorWorkerTest(i);
+                workerThreads[i] = new Thread(workers[i]);
+                workerThreads[i].start();
+            }
+
+            for (int i = 0; i < workerThreads.length; i++) {
+                workerThreads[i].join();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        WorkerThreadTest test = new WorkerThreadTest();
+        test.start();
+    }
+
+
+    public Thread[] getWorkerThreads() {
+        return workerThreads;
+    }
+
+    public selectorWorkerTest[] getWorkers() {
+        return workers;
+    }
+
+
+
+
+}
